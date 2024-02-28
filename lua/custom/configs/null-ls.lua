4f5f0287c085a9fa030f88ba610d16e47b1bffa0
Leonardo Blautzik <leoblautzik@gmail.com>
2024-02-28 13:18:33 -0300

    Arreglando nvdash y notificacion de ruff

--- lua/custom/configs/null-ls.lua
+++ lua/custom/configs/null-ls.lua
@@ -10,7 +10,7 @@ local opts = {
     null_ls.builtins.formatting.golines,
     null_ls.builtins.formatting.black,
     null_ls.builtins.diagnostics.mypy,
-    null_ls.builtins.diagnostics.ruff,
+    --null_ls.builtins.diagnostics.ruff,
   },
   on_attach = function(client, bufnr)
     if client.supports_method "textDocument/formatting" then
